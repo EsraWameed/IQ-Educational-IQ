77d95fe6b91819eaa6d007d461807c243f767e51
Esra Al <esra.wameed@yahoo.com>
2023-07-25 19:06:45 -0400

    fixed labels

--- userInfo.sql
+++ userInfo.sql
@@ -1,5 +1,5 @@
 /* created table to collect information about users*/
-CREATE TABLE users (
+CREATE TABLE students (
     id INT AUTO_INCREMENT PRIMARY KEY,
     username VARCHAR(50) NOT NULL,
     email VARCHAR(100) NOT NULL,
@@ -7,16 +7,16 @@ CREATE TABLE users (
 );
 
 /* created table to collect information about users*/
-CREATE TABLE posts (
+CREATE TABLE notes (
     id INT AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(100) NOT NULL,
     content TEXT,
-    user_id INT NOT NULL,
-    FOREIGN KEY (user_id) REFERENCES users(id)
+    student_id INT NOT NULL,
+    FOREIGN KEY (student_id) REFERENCES students(id)
 );
 /* to create indexes on some columns for better query performance */
-CREATE INDEX idx_users_username ON users (username);
+CREATE INDEX idx_users_username ON students (username);
 
 /* inclding initial data to script*/
-INSERT INTO users (username, email, password) VALUES
+INSERT INTO students (username, email, password) VALUES
     ('Esra_Al', 'esra@yahoo.com', 'some_random_pass');
